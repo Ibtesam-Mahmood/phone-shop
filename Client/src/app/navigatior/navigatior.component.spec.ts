81216b239d8ab95cb45a15fe975d084e436aafa7
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2019-11-30 03:06:35 -0500

    basic nav and home components made

---
+++ Client/src/app/navigatior/navigatior.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavigatiorComponent } from './navigatior.component';
+
+describe('NavigatiorComponent', () => {
+  let component: NavigatiorComponent;
+  let fixture: ComponentFixture<NavigatiorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NavigatiorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavigatiorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Client/src/app/navigatior/navigatior.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+//The persistant navigator above the page content
+
+@Component({
+  selector: 'app-navigatior',
+  templateUrl: './navigatior.component.html',
+  styleUrls: ['./navigatior.component.scss']
+})
+export class NavigatiorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
